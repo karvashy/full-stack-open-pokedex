2780a5518e8a8771cc07708d2e649ce8b8f13388
karvashy <karvash991@gmail.com>
2023-10-28 19:06:01 +0530

    finish 11.9

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -5,7 +5,8 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('venusaur page can be opened', function() {
-    cy.visit('http://localhost:5000/pokemon/venusaur')
+    cy.visit('http://localhost:5000')
+    cy.contains('venusaur').click()
     cy.contains('venusaur')
   })
 })
